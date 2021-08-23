f9dd7213240c5dc33c23eaefa709bbdf3034e6e5
Nestor <ncervan87@gmail.com>
2021-08-23 08:27:53 -0700

    adds text to body

--- projects/mylib/src/lib/mylib.component.ts
+++ projects/mylib/src/lib/mylib.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'ml-mylib',
-  template: ` <p>mylib works!</p> `,
+  template: ` <h1>mylib works!</h1> `,
   styles: [],
 })
 export class MylibComponent implements OnInit {
