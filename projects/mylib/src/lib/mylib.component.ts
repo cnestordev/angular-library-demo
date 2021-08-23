ba2ac64a9959294dc76a4e3d6e801a6c0cc9f7b6
Nestor <ncervan87@gmail.com>
2021-08-23 08:53:06 -0700

    adds functionality to lib

--- projects/mylib/src/lib/mylib.component.ts
+++ projects/mylib/src/lib/mylib.component.ts
@@ -1,12 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+  ViewEncapsulation,
+} from '@angular/core';
 
 @Component({
   selector: 'ml-mylib',
-  template: ` <h1>mylib works!</h1> `,
-  styles: [],
+  template: `
+    <div class="container">
+      <input type="text" (keydown.enter)="markText($event.target)" />
+      <div #content [hidden]="true">
+        <ng-content></ng-content>
+      </div>
+      <div [innerHTML]="controlledContent"></div>
+    </div>
+  `,
+  styles: [
+    `
+      .mark {
+        background-color: yellow;
+      }
+    `,
+  ],
+  encapsulation: ViewEncapsulation.None,
 })
 export class MylibComponent implements OnInit {
+  @ViewChild('content') content!: ElementRef;
+
+  originalContent!: string;
+  controlledContent!: string;
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    // Created a setTimeout to give time for the content to load
+    setTimeout(() => {
+      this.controlledContent = this.originalContent =
+        this.content.nativeElement.textContent;
+    }, 2000);
+  }
+
+  markText(value: any) {
+    this.controlledContent = this.originalContent;
+    this.controlledContent = this.originalContent.replace(
+      new RegExp(value.value, 'g'),
+      `<span class="mark">${value.value}</span>`
+    );
+  }
 }
